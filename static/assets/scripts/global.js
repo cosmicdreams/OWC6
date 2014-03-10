d23312a5b3825fa071994d1090e198608df26b12
Noah Blon <nblon@nerdery.com>
2014-03-10 10:01:41 -0500

    Initial FE boilerplate.

---
+++ static/assets/scripts/global.js
@@ -0,0 +1,19 @@
+/* ---------------------------------------------------------------------
+Global JavaScript
+
+Target Browsers: All
+Authors: Noah Blon
+------------------------------------------------------------------------ */
+
+// Namespace Object
+var NERD = NERD || {};
+
+// Pass reference to jQuery and Namespace
+(function($, APP) {
+
+    // DOM Ready Function
+    $(function() {
+    });
+
+
+}(jQuery, NERD));
