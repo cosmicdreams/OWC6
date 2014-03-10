3493f09d8f0caa519416ad47435afd2b2bc48e03
Noah Blon <nblon@nerdery.com>
2014-03-10 09:57:06 -0500

    Initial FE boilerplate commit.

---
+++ static/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- META DATA -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!--[if IE]>
+        <meta name="MSSmartTagsPreventParsing" content="true" />
+        <meta http-equiv="imagetoolbar" content="no" />
+        <meta http-equiv="cleartype" content="on" />
+        <![endif]-->
+
+        <!-- SEO -->
+        <title>OWC6</title>
+
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
+
+        <!-- JQUERY -->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
+
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="assets/scripts/global.js"></script>
+    </head>
+<body>
+
+</body>
+</html>
