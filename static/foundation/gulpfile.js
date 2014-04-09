b3bd645e50128c2a6967e9db9c2134e176df0243
Noah Blon <noahblon@gmail.com>
2014-04-09 08:48:01 -0500

    Building out foundation style guide components

--- static/foundation/gulpfile.js
+++ static/foundation/gulpfile.js
@@ -2,7 +2,7 @@ var BASE_PATH = './';
 var SOURCE_PATH = 'src';
 var DIST_PATH = 'dist';
 
-var CSS_SOURCE_PATH = '<%= BASE_PATH %>' + 'assets/styles/';
+var CSS_SOURCE_PATH = BASE_PATH + 'assets/styles/';
 
 /* GULP PLUGINS */
 var gulp = require('gulp');
@@ -16,34 +16,34 @@ var connect = require('gulp-connect');
 /* GULP TASKS */
 
 // Process styles
-gulp.task('styles', function() {
-    return gulp.src(BASE_PATH + SOURCE_PATH + '/scss/foundation.scss')
-        .pipe(sass({ style: 'expanded' }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest(BASE_PATH + DIST_PATH + '/assets/css'))
-        ;
-});
+// gulp.task('styles', function() {
+//     return gulp.src(BASE_PATH + SOURCE_PATH + '/scss/foundation.scss')
+//         .pipe(sass({ style: 'expanded' }))
+//         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//         .pipe(gulp.dest(BASE_PATH + DIST_PATH + '/assets/css'))
+//         ;
+// });
 
 gulp.task('hologram', function() {
-    gulp.src('./**/**')
+    gulp.src('')
         .pipe(exec('hologram'));
 });
 
 gulp.task('connect', function() {
   connect.server({
     root: ['styleguide'],
-    port: 1338,
+    port: 1337,
     livereload: false
   });
 });
 
-gulp.task('html', function () {
-  gulp.src('./styleguide/**/*.html')
-    .pipe(connect.reload());
-});
+// gulp.task('html', function () {
+//   gulp.src('./styleguide/**/*.html')
+//     .pipe(connect.reload());
+// });
 
-gulp.task('watch', function () {
-  gulp.watch(['./styleguide/**/*.html'], ['html']);
-});
+// gulp.task('watch', function () {
+//   gulp.watch(['./styleguide/**/*.html'], ['html']);
+// });
 
-gulp.task('default', ['connect', 'watch']);
+// gulp.task('default', ['connect', 'watch']);
