e5c05706e6df56dbe81288715cb6d93f9ef62cfa
Noah Blon <nblon@nerdery.com>
2014-04-12 17:16:54 -0500

    Porting over build system.

---
+++ sites/all/themes/homeforlife/gulpfile.js
@@ -0,0 +1,92 @@
+/* GULP PLUGINS */
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var exec = require('gulp-exec');
+var sass = require('gulp-ruby-sass');
+
+/* GULP TASKS */
+
+gulp.task('clean-styles', function() {
+    gulp.src('./assets/styles/*.css', {read: false})
+        .pipe(clean());
+});
+
+
+gulp.task('clean-styleguide', function() {
+  gulp.src('./styleguide/objects.html', {read: false})
+    .pipe(clean());
+  gulp.src('./styleguide/wysiwyg.html', {read: false})
+    .pipe(clean());
+  gulp.src('./styleguide/css/*.css', {read: false})
+    .pipe(clean());
+  gulp.src('./styleguide/scripts/*', {read: false})
+    .pipe(clean());
+});
+
+
+gulp.task('hologram', function() {
+    gulp.src('')
+        .pipe(exec('hologram'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['./'],
+    port: 1337,
+    livereload: false
+  });
+});
+
+//gulp.task('move-samples', function() {
+//    gulp.src('templates/samples/*.html')
+//        .pipe(gulp.dest('./'))
+//});
+
+// gulp.task('html', function () {
+//    gulp.src('./styleguide/**/*.html')
+//      .pipe(connect.reload());
+// });
+
+// gulp.task('watch', function () {
+//    gulp.watch(['./styleguide/**/*.html'], ['html']);
+// });
+
+// gulp.task('default', ['connect', 'watch']);
+
+gulp.task('theme-sass', function() {
+  gulp.src('./scss/homeforlife.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./styleguide/css'))
+    ;
+});
+
+gulp.task('custom-sass', function() {
+  gulp.src('./scss/custom/main.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./styleguide/css'))
+    ;
+});
+
+gulp.task('copy-script', function() {
+  gulp.src('./js/*')
+    .pipe(gulp.dest('./styleguide/scripts'))
+    ;
+});
+
+
+
+gulp.task('build', function() {
+  gulp.run('clean-styles');
+  gulp.run('clean-styleguide');
+  gulp.run('theme-sass');
+  gulp.run('custom-sass');
+  gulp.run('copy-script');
+  gulp.run('hologram');
+});
