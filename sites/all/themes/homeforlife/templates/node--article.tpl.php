a95221943cdc594fe8bab8104c7e6f3def5d252d
David Needham <david@enjoycreativity.com>
2014-04-13 06:35:14 -0500

    theming the article

--- sites/all/themes/homeforlife/templates/node--article.tpl.php
+++ sites/all/themes/homeforlife/templates/node--article.tpl.php
@@ -75,37 +75,52 @@
  * @see template_process()
  */
 ?>
-<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<article id="node-<?php print $node->nid; ?>" class="row <?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
+  <div class="small-12 columns">
+    <?php print render($title_prefix); ?>
     <?php if (!$page): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
     <?php endif; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="posted">
+    <?php if ($display_submitted): ?>
+    <h4 class="post-sub-header posted">
       <?php if ($user_picture): ?>
         <?php print $user_picture; ?>
       <?php endif; ?>
       <?php print $submitted; ?>
-    </div>
+    </h4>
   <?php endif; ?>
+  </div>
 
-  <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
 
-  ?>
 
-  <?php if (!empty($content['field_tags']) && !$is_front): ?>
-    <?php print render($content['field_tags']) ?>
-  <?php endif; ?>
+    <div class="row">
+      <div class="small-8 columns">
+        <?php
+        dsm($content);
+          // We hide the comments and links now so that we can render them later.
+          hide($content['comments']);
+          hide($content['links']);
+          hide($content['field_image']);
+          hide($content['field_tags']);
+          print render($content);
+
+        ?>
+      </div>
+
+      <div class="small-4 columns">
+        <?php if (!empty($content['field_image']) && !$is_front): ?>
+        <?php print render($content['field_image']) ?>
+        <?php endif; ?>
+        <?php if (!empty($content['field_tags']) && !$is_front): ?>
+          <?php print render($content['field_tags']) ?>
+        <?php endif; ?>
+      </div>
+</div>
 
   <?php print render($content['links']); ?>
   <?php print render($content['comments']); ?>
